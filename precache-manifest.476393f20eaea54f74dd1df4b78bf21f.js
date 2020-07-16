11bc6c477f369cac07905117bb0b038eca9c6cae
Vincent CHAPRON <vincent.chapron@iziwork.com>
2020-07-16 21:29:51 +0200

    first commit

---
+++ precache-manifest.476393f20eaea54f74dd1df4b78bf21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221ab793540b67e4cc95ebac3b10ad23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dda7a7f4a84550565e04",
+    "url": "/static/css/main.4a4efdf1.chunk.css"
+  },
+  {
+    "revision": "c31903990631c35baa73",
+    "url": "/static/js/2.35aead78.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.35aead78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dda7a7f4a84550565e04",
+    "url": "/static/js/main.d2b0715c.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  }
+]);
